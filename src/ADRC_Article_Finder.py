f9aeb382a12e623008e49bd0eb9952dc39c97c26
David Gutman <dagutman@gmail.com>
2014-04-16 17:23:36 -0400

    patched author parser to deal with collectivegroupname

--- src/ADRC_Article_Finder.py
+++ src/ADRC_Article_Finder.py
@@ -10,14 +10,6 @@ from Bio.Entrez import efetch, read
 # AUTHOR LOCATION
 # Author University
 
-Entrez.email = "dagutman@gmail.com"     # Always tell NCBI who you are
-
-adrc_file_list = 'adrc_pubs.txt'
-adrc_fp = open('input_data/emory_adrc_pubs_v1.txt','r')
-adrc_raw_paper_list = adrc_fp.readlines()
-
-fp_out = open('input_data/need_pmid.txt','w')
-
 global_author_list = {}   ### I am using this list of authors to then build a connectivity matrix--- but first I need to see who's represented
 
 ### the input file in this case is a text document with PMID or PMCID's
@@ -33,8 +25,6 @@ global_author_list = {}   ### I am using this list of authors to then build a co
  DOI: enter the complete string, e.g., 10.1093/nar/gks1195.
 """
 
-pub_list = [ pub.strip('\n') for  pub in adrc_raw_paper_list if len(pub)>1 ]   ## turns it into a list and removes the blank spaces
-
 pmid_regex = re.compile('PMID:*\s*(\d+)') ## matches 0 or more spaces
 pmcid_regex = re.compile('PMCID:*\s*(PMC\d+)')
 debug = False
@@ -42,34 +32,6 @@ PMID_LIST = []
 PMCID_LIST = []
 not_matched = 0
 
-#  Scan through file line by line looking for PMID or PMCID--- each reference is only on a single line
-for pub in pub_list:
-    found_PMID = False
-    if 'PMID' in pub:
-    #    print pub
-        m = pmid_regex.search(pub)
-        if m:
-            if debug: print m.group(0),m.group(1)
-            PMID_LIST.append(m.group(1))
-            found_PMID = True
-        else:
-            print "PMID found but pattern not parsed properly...",pub
-    elif 'PMCID' in pub and not found_PMID:
-        if debug: print "SEarching for PMCID"
-        m = pmcid_regex.search(pub)
-        if m:
-            if debug: print m.group(0),m.group(1)
-            PMCID_LIST.append(m.group(1))
-            found_PMID = True
-        else:
-            print "PMID found but pattern not parsed properly...",pub
-            
-    else:
-        print "Did not find PMID or PMCID",pub
-	fp_out.write(pub+'\n')        
-	not_matched += 1
-        
-print not_matched,"entries did not have a PMC or PMID"
 
 def format_ddate(ddate):
     """Turn a date dictionary into an ISO-type string (YYYY-MM-DD)."""
@@ -82,25 +44,32 @@ def format_ddate(ddate):
             return None
     return "%s-%s-%s" % (year, month.zfill(2), day.zfill(2))
 
-def get_metadata_from_PMID( pmid ):
+def get_metadata_from_PMID( pmid, output_errors=False, dump_xml=False ):
     """This function will take an input PMID and parse the attributes I am interested in for the cytoscape plugin...."""
     handle = efetch(db='pubmed', id=pmid, retmode='xml')
     xml_data = read(handle)[0]
     verbose_output = False
+#    output_errors= False
+
     try:
         date_completed = format_ddate( xml_data['MedlineCitation']['DateCompleted'] )
     except:
-        print "Data Completed not available??",pmid
+        print "Date Completed not available??",pmid
+	## Will try date created	
+	date_completed = None
+	date_created = format_ddate( xml_data['MedlineCitation']['DateCreated'] )
+	#fp_error.write('Date Completed Not Avaiable:\n'+str(xml_data)+'\n\n')    
 
     try:
         otherID = xml_data['MedlineCitation']['OtherID']
     except:
         print "Other ID Not availble??",pmid
+
     try:
         MeshHeadings = xml_data['MedlineCitation']['MeshHeadingList']
     except:
         print "Unable to get mesheadings for",pmid
-    
+	if output_errors: fp_error.write('MESH NOT AVAIABLE:\n'+str(xml_data)+'\n\n')    
     
     try:
         article = xml_data['MedlineCitation']['Article']
@@ -108,26 +77,38 @@ def get_metadata_from_PMID( pmid ):
         #print date_completed,otherID
         for author in  article['AuthorList']:
             #author_key = { 'LastNAme': author['LastName'], 'Initials': author['Initials'] }
-            author_key =    author['LastName'] + ','+  author['Initials'] 
+	    if 'LastName' in author:
+        	author_key =    author['LastName'] + ','+  author['Initials'] 
             #print author['LastName'],author['Initials'],author,'MOO'
-            if author_key in global_author_list:
-                global_author_list[ author_key ] +=1
+	        if author_key in global_author_list:
+          	    global_author_list[ author_key ] +=1
                 #print "adding author"
-            else:
-                global_author_list[ author_key ] = 1
+	        else:
+        	    global_author_list[ author_key ] = 1
+	    elif 'CollectiveName' in author:
+		print "FOUND A COLLECTION EXAMPLE",author
                 #print "I ADDED AN AUTHOR..."
         #return abstract
     except IndexError:
         return None
     except:
-        print "unable to process",pmid
-        print "Unexpected error:", sys.exc_info()[0]
+        print "unable to proces article tag",pmid
+#        print "Unexpected error:", sys.exc_info()[0]
+	if output_errors: fp_error.write('Article NOT AVAILABLE\n'+str(xml_data)+'\n\n')    
+
 
     try:
+
         abstract = article['Abstract']['AbstractText'][0]
     except:
         print "Unable to get abstract for",pmid
-        print "Unexpected error:", sys.exc_info()[0]
+
+    if dump_xml:
+        print xml_data
+	return xml_data		
+
+        #print "Unexpected error:", sys.exc_info()[0]
+	#fp_error.write(str(xml_data)+'\n')    
 
 ### Gets Med Line Citation 
 #MeshHeadings = medline_citation['MeshHeadingList']
@@ -180,8 +161,50 @@ def get_metadata_from_PMID( pmid ):
 #handle = efetch(db='pubmed', id=broken_pmid, retmode='xml')
 #xml_data = read(handle)[0]
 
+if __name__ == "__main__":
 
-for pmid in PMID_LIST:
-    print pmid
-    get_metadata_from_PMID(pmid)
+    Entrez.email = "dagutman@gmail.com"     # Always tell NCBI who you are
+    adrc_file_list = 'adrc_pubs.txt'
+
+    adrc_fp = open('input_data/emory_adrc_pubs_v1.txt','r')
+    adrc_raw_paper_list = adrc_fp.readlines()
+
+    fp_out = open('input_data/need_pmid.txt','w')
+
+    fp_error = open('input_data/parsing_issues.txt','w')
+    pub_list = [ pub.strip('\n') for  pub in adrc_raw_paper_list if len(pub)>1 ]   ## turns it into a list and removes the blank spaces
+
+#  Scan through file line by line looking for PMID or PMCID--- each reference is only on a single line
+    for pub in pub_list:
+        found_PMID = False
+        if 'PMID' in pub:
+    #    print pub
+            m = pmid_regex.search(pub)
+            if m:
+                if debug: print m.group(0),m.group(1)
+                PMID_LIST.append(m.group(1))
+                found_PMID = True
+            else:
+                print "PMID found but pattern not parsed properly...",pub
+        elif 'PMCID' in pub and not found_PMID:
+            if debug: print "SEarching for PMCID"
+            m = pmcid_regex.search(pub)
+            if m:
+                if debug: print m.group(0),m.group(1)
+                PMCID_LIST.append(m.group(1))
+                found_PMID = True
+            else:
+                print "PMID found but pattern not parsed properly...",pub
+            
+        else:
+            print "Did not find PMID or PMCID",pub
+    	    fp_out.write(pub+'\n')        
+            not_matched += 1
+        
+    print not_matched,"entries did not have a PMC or PMID"
+
+
+    for pmid in PMID_LIST:
+       print pmid
+       get_metadata_from_PMID(pmid)
 
