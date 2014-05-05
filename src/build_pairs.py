53395237d31cb12632b13e0b29c7bdfb144c6313
David Gutman <dagutman@gmail.com>
2014-05-05 15:08:06 -0400

    adding info on author affil to grid

--- src/build_pairs.py
+++ src/build_pairs.py
@@ -25,121 +25,165 @@ affil_fp = open('affiliations_rawtext.csv','w')
 cytoscape_fp = open('cytoscape_simpleformat.csv','w')
 
 
+curated_author_affils_fp = open('affiliations_manually_curated.csv','r')
+
+
+
+curated_author_data = curated_author_affils_fp.readlines()
+
+curated_author_datadict = {}
 ## Build unique author list
-author_occurrence_hash = {}
-author_pairs = []
+##The format of this file should be  pmid;#;Lastname;#;FirstName;#;Initials;#;Affiliation;#;University;#;State;#;City;#;
+university_index = []
 
 
+for line in curated_author_data:
+	cols = line.rstrip().split(';')
+	pmid = cols[1]
+	lastname = cols[3]
+	firstname = cols[5]
+	initials = cols[7]
+	affil_string = cols[9]
+	university = cols[11]
+	state = cols[13]
+	city = cols[15]
+	if university:
+#		print pmid,state,city
+		author_initials = lastname+ ','+  initials
+		if author_initials not in curated_author_datadict:
+			curated_author_datadict[author_initials] = { 'lastname': lastname, 'firstname':firstname, 'university':university, 'state': state, 'city': city }
+			if university not in university_index:
+				university_index.append(university)
+		else:
+			cur_university_info = curated_author_datadict[author_initials]['university']
+			if university != cur_university_info:
+				print "author mismatch???",cols
+				print university,cur_university_info
 
-for pmid in pmid_datahash:
-    author_list = pmid_datahash[pmid]['auth_list']
-    print pmid_datahash[pmid]
-    pub_year = pmid_datahash[pmid]['publication_date'].split('-')[0]
-    
-    print author_list
-    
-    author_pairs.extend( list( itertools.combinations(author_list,2) ) )
-#    print author_pairs
-    for author in author_list:
-	if author in author_occurrence_hash:
-		author_occurrence_hash[author] +=1
-	else:
-		author_occurrence_hash[author] = 1
-    affiliation_list = pmid_datahash[pmid]['affiliations']
+##Unique Author List--- I need to figure out how many universities are present, and assign an ID to each one
+
+pprint( university_index)
+print len(university_index)
+
+
+def extract_affiliation_info( pmid_datahash):
+    """This subfunction will extract the author affiliation info and dump this to a file for subsequent curation """
+    for pmid in pmid_datahash:  
+	affiliation_list = pmid_datahash[pmid]['affiliations']
     #print affiliation_list
-    for affil in affiliation_list:
+	for affil in affiliation_list:
 	    print type(affil)
 	    author_info_string = "pmid;%s;LastName;%s;FirstName;%s;Initials;%s;Affiliation;%s;University;;State;;City;;" % (  pmid, affil[0]['LastName'], affil[0]['ForeName'],affil[0]['Initials'],affil[1])
-	    print author_info_string
-
-#    affil_fp.write('pmid:'+pmid+';'+str(affiliation_list)+'\n')
+	    #print author_info_string
             affil_fp.write(author_info_string.encode('utf-8')+'\n')
 
-for a in author_occurrence_hash:
-	print a,author_occurrence_hash[a]    
 
-print len(author_occurrence_hash),"authors are present in this stack"
-print len(author_pairs)
+
+def build_author_pairs_by_year ( pmid_datahash, year_to_analyze):
+    author_occurrence_hash = {}
+    author_pairs = []
+
+    for pmid in pmid_datahash:
+	author_list = pmid_datahash[pmid]['auth_list']
+	#print pmid_datahash[pmid]
+	pub_year = pmid_datahash[pmid]['publication_date'].split('-')[0]
+    
+	#print author_list
+#        print pub_year,year_to_analyze
+	if int(pub_year) == year_to_analyze:
+       	    author_pairs.extend( list( itertools.combinations(author_list,2) ) )
+#    print author_pairs
+  	    for author in author_list:
+	        if author in author_occurrence_hash:
+		    author_occurrence_hash[author] +=1
+   	        else:
+		    author_occurrence_hash[author] = 1
+    return( author_occurrence_hash, author_pairs) 
 
 #print len(set(author_pairs)),"author pairs present"
 ### NEED TO REMOVE DUPLICATE AUTHOR PAIRS.. e.g.  Lah, Levey == Levey, Lah
-
 #pprint(author_occurrence_hash)
 
+def generate_cooccurrence_info( author_pairs, author_occurrence_hash):
+	author_cooccurrence_hash = Counter(author_pairs) ### now also need to generate the list of cooccurrence
 
-author_cooccurrence_hash = {}  ### now also need to generate the list of cooccurrence
+	min_paper_count = 1
 
-author_cooccurrence_hash = Counter(author_pairs)
+	filtered_author_list = []
+	filtered_author_dict = {}
 
-min_paper_count = 1
+	author_id = 0  ## Need to define a unique ID for each author so I can then link them
+	for k in reversed(sorted(author_occurrence_hash.items(), key=lambda t: t[1])):
+		(author,count) = k
+		if count>=min_paper_count: 
+			print k
+			filtered_author_list.append(k)
+			filtered_author_dict[author] = author_id
+			author_id +=1
 
-filtered_author_list = []
-filtered_author_dict = {}
+	print len(filtered_author_list),"authors had more than",min_paper_count,"pubs"
 
-author_id = 0  ## Need to define a unique ID for each author so I can then link them
-for k in reversed(sorted(author_occurrence_hash.items(), key=lambda t: t[1])):
-	(author,count) = k
-	if count>=min_paper_count: 
-		print k
-		filtered_author_list.append(k)
-		filtered_author_dict[author] = author_id
-		author_id +=1
+	node_list = []
+	for k in sorted(filtered_author_dict.items(), key = lambda t: t[1]):
+		(author_name,author_uid) = k
+		if author_name in curated_author_datadict:
+			print curated_author_datadict[author_name]['university'],author_uid,author_name
+		else:
+			print author_name.encode('utf-8'),"not in datadict??!"
 
-print len(filtered_author_list),"authors had more than",min_paper_count,"pubs"
+		
+		## group_id is used for colors.. right now I only have 20 colors to choose from though..
+		try:  
+			author_university = curated_author_datadict[author_name]['university']
+			group_id = university_index.index(author_university)
+			node_list.append( { 'name':author_name, 'group': group_id % 20 } )
 
-node_list = []
-for k in sorted(filtered_author_dict.items(), key = lambda t: t[1]):
-	(author_name,author_uid) = k
-	node_list.append( { 'name':author_name, 'group': author_uid % 20 } )
+		except:
+			group_id = 0
+#			print 'NEED;---;',curated_author_datadict[author_name]
+			#print author_name.encode('utf-8'),'NEED'
 
-print node_list
+	#print node_list
 
-for k in sorted(filtered_author_dict.items(), key = lambda t: t[1]):
-	(author_name, author_uid) = k
-	print author_name
+	for k in sorted(filtered_author_dict.items(), key = lambda t: t[1]):
+		(author_name, author_uid) = k
+		#print author_name
 
 ## So now for a given author i... I want to see how many papers he/she has with author K...
 ## Need to make sure this recursion is actually correct
 
-link_list = []
+	link_list = []
 
-for i in range (0,len(filtered_author_dict) ):
-	for k in range(i+1,len(filtered_author_dict) -1 ):
-		#print i,filtered_author_list[i],k,filtered_author_list[k]
-		author_cooccurrence_count  = 0
-		(a1,pub_count_a1) = ( filtered_author_list[i] )
-		(a2,pub_count_a2) = ( filtered_author_list[k ] )
-		#print author_pairs_count
-		if author_cooccurrence_hash[ (a1,a2)  ]:
-			#print 'found it??',a1,a2, author_cooccurrence_hash[ (a1,a2) ]
-			author_cooccurrence_count = author_cooccurrence_hash[ (a1,a2) ] 
-		## Since ( a1, a2 ) == ( a2, a1) in this case, need to check for both possibilities
-		if author_cooccurrence_hash[ (a2,a1)  ]:
-			#print 'found it??',a1,a2, author_cooccurrence_hash[ (a2,a1) ]
-			author_cooccurrence_count = author_cooccurrence_hash[ (a2,a1) ] 
-		if author_cooccurrence_count > 0:
-			#print a1,a2,"publish together",author_cooccurrence_count
-			#print "Source",i,"target",k,"value",author_cooccurrence_count
-			node_info = { 'source': i, 'target': k, 'value': author_cooccurrence_count }
-#			print node_info
-			link_list.append(node_info)
-			author_linkage_string = '"%s";%d;"%s"' % ( filtered_author_list[i][0], author_cooccurrence_count, filtered_author_list[k][0])
-			cytoscape_fp.write( author_linkage_string + '\n' )
+	for i in range (0,len(filtered_author_dict) ):
+		for k in range(i+1,len(filtered_author_dict) -1 ):
+			#print i,filtered_author_list[i],k,filtered_author_list[k]
+			author_cooccurrence_count  = 0
+			(a1,pub_count_a1) = ( filtered_author_list[i] )
+			(a2,pub_count_a2) = ( filtered_author_list[k ] )
+			#print author_pairs_count
+			if author_cooccurrence_hash[ (a1,a2)  ]:
+				#print 'found it??',a1,a2, author_cooccurrence_hash[ (a1,a2) ]
+				author_cooccurrence_count = author_cooccurrence_hash[ (a1,a2) ] 
+			## Since ( a1, a2 ) == ( a2, a1) in this case, need to check for both possibilities
+			if author_cooccurrence_hash[ (a2,a1)  ]:
+				#print 'found it??',a1,a2, author_cooccurrence_hash[ (a2,a1) ]
+				author_cooccurrence_count = author_cooccurrence_hash[ (a2,a1) ] 
+			if author_cooccurrence_count > 0:
+				#print a1,a2,"publish together",author_cooccurrence_count
+				#print "Source",i,"target",k,"value",author_cooccurrence_count
+				node_info = { 'source': i, 'target': k, 'value': author_cooccurrence_count }
+#				print node_info
+				link_list.append(node_info)
+				author_linkage_string = '"%s";%d;"%s"' % ( filtered_author_list[i][0], author_cooccurrence_count, filtered_author_list[k][0])
+				cytoscape_fp.write( author_linkage_string.encode('utf-8') + '\n' )
+	
 
-#    {"source":1,"target":0,"value":1},
-
-#pprint(link_list)
-
-json_object = { 'nodes':node_list, 'links': link_list}
-pprint( json_object)
+	json_object = { 'nodes':node_list, 'links': link_list}
+	#pprint( json_object)
+	json.dump(json_object, js_fp)
 
 ## NOW I NEED TO GENERATE THE LINK LIST...
-json.dump(json_object, js_fp)
-
-
-
-#pprint( filtered_author_dict)
-
+	
 
 ### NOW ACTUALLY BUILD THE JSON OBJECT WHICH CONSISTS OF A LIST OF NODES AND LINKS
 
@@ -157,9 +201,6 @@ json.dump(json_object, js_fp)
 #    {"source":2,"target":0,"value":8},
 #    {"source":3,"target":0,"value":10},
 #    {"source":3,"target":2,"value":6},
-#    {"source":4,"target":0,"value":1},
-#    {"source":5,"target":0,"value":1},
-#    {"source":6,"target":0,"value":1},
 ##    {"source":7,"target":0,"value":1},
 #    {"source":76,"target":58,"value":1},
 #  ]
@@ -172,21 +213,17 @@ json.dump(json_object, js_fp)
 
 #"""The service can provide output in a number of formats, as specified by the format parameter, which can be one of "html", "xml", "json", or "csv". "xml" is the dafault, and several examples of this response format are given above. Examples of each of the other formats is shown below.
 
-""">>> from collections import OrderedDict
 
->>> # regular unsorted dictionary
->>> d = {'banana': 3, 'apple':4, 'pear': 1, 'orange': 2}
 
->>> # dictionary sorted by key -- OrderedDict(sorted(d.items()) also works
->>> OrderedDict(sorted(d.items(), key=lambda t: t[0]))
-OrderedDict([('apple', 4), ('banana', 3), ('orange', 2), ('pear', 1)])
 
->>> # dictionary sorted by value
->>> OrderedDict(sorted(d.items(), key=lambda t: t[1]))
-OrderedDict([('pear', 1), ('orange', 2), ('banana', 3), ('apple', 4)])
 
->>> # dictionary sorted by length of the key string
->>> OrderedDict(sorted(d.items(), key=lambda t: len(t[0])))
-OrderedDict([('pear', 1), ('apple', 4), ('orange', 2), ('banana', 3)])
-"""
+years_to_scan = [2012]
+
+for year in years_to_scan:
+    (author_occurrence_hash, author_pairs ) = build_author_pairs_by_year (pmid_datahash, year)
+    print len(author_occurrence_hash),"authors are present in this stack",year
+    print len(author_pairs),"pairs for year",year
+
+    generate_cooccurrence_info(author_pairs, author_occurrence_hash)
+
 
